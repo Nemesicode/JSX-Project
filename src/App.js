3f2ffd036c04a8f470ad14cb56a7dab1aef0b65e
Nemesicode <abdoulayediallomail@gmail.com>
2021-11-29 11:10:46 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import imageInSrc from "./imageInSrc.jpg"
+import "./style.css"
 
-function App() {
+const App=() => {
   return (
-    <div className={"App"}>
-      <header className={"App-header"}>
-        <img src={logo} className={"App-logo"} alt={"logo"} />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={"App-link"}
-          href={"https://reactjs.org"}
-          target={"_blank"}
-          rel={"noopener noreferrer"}
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div style={{ border:"solid 1px black", maxWidth:"100vw"}}>
+
+      <h1 class="title">Your name here</h1>
+
+        <br/ >
+
+        <img src={imageInSrc} alt="screenshot" />
+
+          <br/ >
+
+ <img src="/imageInPublic.jpg" alt="screenshot" />
+
+</div>
+
+<video width="320" height="240" controls>
+
+ <source src="myVideo.mp4" type="video/mp4" />
+
+</video>
+</>
   );
 }
 
